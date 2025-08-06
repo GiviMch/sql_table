11bd493d1e4ce6e9ac3bdde1e745871ee4e209ed
GiviMch <geva1992@yandex.ru>
2025-08-06 23:00:07 +0300

    обновил файл таблицы с добавлением персон

--- sql_table.sql
+++ sql_table.sql
@@ -8,3 +8,7 @@ CREATE TABLE PERSONS (
     city_of_living VARCHAR(50),
     PRIMARY KEY (name, surname, age)
 );
+USE PERSONS;
+INSERT INTO PERSONS VALUES ('Ivan', 'Ivanov', 30, '123456789', 'MOSCOW');
+INSERT INTO PERSONS VALUES ('Maria', 'Petrova', 25, '987654321', 'SPB');
+INSERT INTO PERSONS VALUES ('Alex', 'Smirnov', 28, '555555555', 'MOSCOW');
